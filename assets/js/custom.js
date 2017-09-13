575148fc61075fbca31c2555e4f82a17198bd5fb
Nandor Kiss <kipal42@gmail.com>
2017-09-13 12:26:04 +0200

    slideToggle instead of toggle.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,7 +1,7 @@
 (function($){
     $(document).ready(function(){
         $(".headline").click(function(){
-            $(this).parent().find(".details").toggle("slow");
+            $(this).parent().find(".details").slideToggle("slow");
         });
     });
 }($));
