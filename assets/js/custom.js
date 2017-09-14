88eab69356d0953156365d4d940644fdd7e4cbe9
Nandor Kiss <kipal42@gmail.com>
2017-09-14 23:11:27 +0200

    acitve class used instead of hover.

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,8 @@
 (function($){
     $(document).ready(function(){
         $(".headline").click(function(){
-            $(this).toggleClass("hover");
-
-            $(this).parent().find(".details").slideToggle("slow", function(){
-               var isHidden = $(this).css("display") == "none";
-	    });
+            $(this).toggleClass("active");
+            $(this).parent().find(".details").slideToggle("slow");
         });
     });
 }($));
